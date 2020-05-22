5692de7bb5564c3704b0a0690f76142914c6250d
calvincurry22 <calvincurry22@gmail.com>
2020-05-22 14:34:40 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int guessesGiven = 0;
+            int secretNum = rand.Next(1, 101);
+            int guessesRemaining = 4;
+            while (guessesGiven < 4)
+            {
+                Console.WriteLine($"Guess a number. Remaining guesses: {guessesRemaining}");
+
+                string userGuess = Console.ReadLine();
+                int userGuessInt = Int32.Parse(userGuess);
+
+                if (secretNum == userGuessInt)
+                {
+                    Console.WriteLine("Correct! You guessed it!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Try Again!");
+                }
+                guessesRemaining--;
+                guessesGiven++;
+            }
+        }
+    }
+}
